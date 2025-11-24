232c66a1a58b4e002197f98c5a0c6fc775b96a4b
RoxanaO9 <josselyn.obando@unach.edu.ec>
2025-11-23 19:32:44 -0500

    comit final

--- Autonomo_U2T2/codigo_original.py
+++ Autonomo_U2T2/codigo_original.py
@@ -2,23 +2,23 @@ import time
 
 def es_primo(num):
     if num < 2:
-        return False
+        return False # Los numeros menores que 2 no son primos
     for i in range(2, int(num**0.5) + 1):
         if num % i == 0:
-            return False
-    return True
+            return False # si existe un divisor, no es primo
+    return True # Si no se encontraron divisores, es primo
 
 def encontrar_primos_en_rango(inicio, fin):
-    primos = []
+    primos = [] # Almacena los numeros primos encontrados
     for num in range(inicio, fin + 1):
         if es_primo(num):
-            primos.append(num)
-    return primos
+            primos.append(num) # Agrega a la lista si es primo
+    return primos # imprime la lista de nuemeros primos encontrados
 
 if __name__ == "__main__":
-    inicio = time.time()
-    primos = encontrar_primos_en_rango(1, 100000)
-    fin = time.time()
-    tiempo_total = fin - inicio
+    inicio = time.time() # Marca el tiempo de inicio
+    primos = encontrar_primos_en_rango(1, 100000) 
+    fin = time.time()  # Marca el tiempo de fin
+    tiempo_total = fin - inicio # Calcula el tiempo total de ejecución
     print(f"Números primos encontrados: {len(primos)}")
     print(f"Tiempo total de ejecución: {tiempo_total:.3f} segundos")
